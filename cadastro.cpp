406aed7984ecd8786a14c0a4420994db3a57e188
Dyego Anjos <107328787+DyegoAnjos@users.noreply.github.com>
2022-10-10 22:15:06 -0300

    Correção de bugs e criação de outros

--- cadastro.cpp
+++ cadastro.cpp
@@ -5,28 +5,39 @@
 #include <string.h> 
 #include <ctype.h>
 
+//10 ativo
+
 struct tcadastro{
 	char nome[50];
 	int cpf, cadeira;
 };
-struct tcadastro cadastro[50];
+struct tcadastro cadastro[];
 
 void linha(){
 	printf("\n------------------------------");
 }
 
-int ordenar(int cont,int auxN, int auxC,char* auxS){
-	int contComparador=0,igual;
+int ordenar(int cont,char* auxS){
+	
+	int contComparador=0,igual,auxN,auxC;
+	
 	while(contComparador < cont){
+		
 		igual = strcmp(cadastro[contComparador].nome,cadastro[contComparador+1].nome);
 		if(igual>0){
+			
 			strcpy(auxS,cadastro[contComparador].nome);
 			auxN=cadastro[contComparador].cpf;
 			auxC=cadastro[contComparador].cadeira;
+			
 			strcpy(cadastro[contComparador].nome,cadastro[contComparador+1].nome);
 			cadastro[contComparador].cadeira=cadastro[contComparador+1].cadeira;
+			cadastro[contComparador].cpf=cadastro[contComparador+1].cpf;
+			
 			strcpy(cadastro[contComparador+1].nome,auxS);
 			cadastro[contComparador+1].cadeira=auxC;
+			cadastro[contComparador+1].cpf=auxN;
+			
 			contComparador=0;
 		}
 			
@@ -36,26 +47,35 @@ int ordenar(int cont,int auxN, int auxC,char* auxS){
 }
 
 int desistencia(int auxN,int cont){
+	
 	for(int i=0;i<=cont;i++){
 		if(cadastro[i].cpf == auxN){
 			for(int j=0;j<cont;j++){
 				if(cadastro[j].cadeira>10){
-					cadastro[i].cpf=cadastro[j].cpf;
-					strcpy(cadastro[i].nome,cadastro[j].nome);
-				}
-			}			
+					cadasy(cadastro[i].nome,cadastro[j].nome);
+					cadtro[i].cpf=cadastro[j].cpf;
+					strcpastro[j].cadeira=cadastro[i].cadeira;
+					
+					
+					return 0;
+				}	
+			}		
 		}
 	}
+	return 0;
 }
 
-
 int main(){
+	
 	int escolha, cont=-1, auxN, auxC;
 	char soun, auxS[50];
-	FILE *arq;
 	
+	FILE *arq;
+
 	setlocale(LC_ALL, "portuguese");
+	
 	while(1){
+		
 		arq =fopen ("Cadastro.txt","r");
 		if(arq == NULL){
 			printf("\nERRO, arquivo não encontrado!\n");
@@ -74,11 +94,14 @@ int main(){
 					system("cls");
 					printf("::::::::::Cadastro::::::::::");
 					printf("\nDigite o seu nome:");
+					
 					do{
 						fflush(stdin);
 						scanf("%s", &auxS);
 						auxN=strlen(auxS);
 					}while(auxN==0);
+					if(strcmp(auxS,"0")==0)
+						break;
 					strcpy(cadastro[cont].nome,auxS);
 					cadastro[cont].nome[0]=toupper(cadastro[cont].nome[0]);
 					
@@ -86,18 +109,19 @@ int main(){
 					scanf("%d", &cadastro[cont].cpf);
 					
 					cadastro[cont].cadeira=cont+1;
-					printf("\nDeseja conitnuar?S/N");
+					/*printf("\nDeseja conitnuar?S/N");
+					
 					soun=toupper(getch());
 					if(soun == 'N')
-						break;
+						break;*/
 				}
 				
 			break;}
 			case 2:{
 					system("cls");
 					printf("::::::::::Listas de Pessoas::::::::::");
-					//Passar para função depois
-					ordenar(cont,auxN,auxC,auxS);
+					
+					ordenar(cont,auxS);
 					for(int i=0;i<=cont;i++){
 						printf("\nNome:%s", cadastro[i].nome);
 						printf("\nCpf:%d", cadastro[i].cpf);
@@ -112,9 +136,8 @@ int main(){
 			case 3:{
 					printf("::::::::::Desistencia::::::::::");
 					printf("\nDigite o Cpf da Desistencia:");
-					scanf("%d",auxN);
+					scanf("%d",&auxN);
 					desistencia(auxN,cont);
-					main();
 			break;}
 			case 0:{
 				return 0;	
